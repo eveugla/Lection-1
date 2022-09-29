9f3862d000c2fb4c8d9da86f5975427424b64c0b
eveugla <pltevaz@mail.ru>
2022-09-29 18:44:04 +0300

    added Example 005 Condition If Else

---
+++ Examples/Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "евгения")
+{
+    Console.WriteLine("Ура, это же Женечка!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
